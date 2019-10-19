c61dffdd7cd9ac9d3af7a357e39efb96ab86e9be
aboyce <adam@aboyce.uk>
2019-10-19 11:54:23 +0100

    Added initial project and helper files.

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  arrowParens: 'always', // Needed for the type in Typescript
+  bracketSpacing: true,
+  jsxBracketSameLine: true
+}
