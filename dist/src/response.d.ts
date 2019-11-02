36426d024fc4c29a15c39bd599da59ce505226ec
aboyce <adam@aboyce.uk>
2019-11-02 19:17:24 +0000

    v0.9.0

--- dist/src/response.d.ts
+++ dist/src/response.d.ts
@@ -3,8 +3,8 @@ import { Response, NextFunction } from 'express';
  * Will return a typed and mocked version of key Express components.
  * mockClear() or clearMockRes() will clear all internal jest functions.
  */
-export declare const getMockRes: <T extends Response>() => {
-    res: T;
+export declare const getMockRes: () => {
+    res: Response;
     next: NextFunction;
     mockClear: () => void;
     clearMockRes: () => void;
