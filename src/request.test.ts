fcf4a6e0a8f0f73f8ad91ec6d44cc4a736c5e349
aboyce <adam@aboyce.uk>
2019-11-02 11:23:05 +0000

    Increased the number of mocked properties for the request.

--- src/request.test.ts
+++ src/request.test.ts
@@ -5,10 +5,10 @@ describe('getMockReq', () => {
   test('accepts no arguments and return default values', () => {
     const testReq = getMockReq()
 
-    // the request contains values for params, query, and body
+    // the request contains values
     expect(testReq).toBeTruthy()
     expect(testReq).toBeInstanceOf(Object)
-    expect(Object.keys(testReq).length).toBe(3)
+    expect(Object.keys(testReq).length).toBe(34)
 
     // req.params is an empty object
     expect(testReq.params).toBeTruthy()
@@ -24,15 +24,114 @@ describe('getMockReq', () => {
     expect(testReq.body).toBeTruthy()
     expect(testReq.body).toBeInstanceOf(Object)
     expect(Object.keys(testReq.body).length).toBe(0)
+
+    // req.cookies is an empty object
+    expect(testReq.cookies).toBeTruthy()
+    expect(testReq.cookies).toBeInstanceOf(Object)
+    expect(Object.keys(testReq.cookies).length).toBe(0)
+
+    // req.method is an empty string
+    expect(testReq.method).toBe('')
+
+    // req.protocol is an empty string
+    expect(testReq.protocol).toBe('')
+
+    // req.secure is a boolean
+    expect(testReq.secure).toBe(false)
+
+    // req.ip is an empty string
+    expect(testReq.ip).toBe('')
+
+    // req.ips is an empty array
+    expect(Array.isArray(testReq.ips)).toBe(true)
+    expect(testReq.ips.length).toBe(0)
+
+    // req.subdomains is an empty array
+    expect(Array.isArray(testReq.subdomains)).toBe(true)
+    expect(testReq.subdomains.length).toBe(0)
+
+    // req.path is an empty string
+    expect(testReq.path).toBe('')
+
+    // req.hostname is an empty string
+    expect(testReq.hostname).toBe('')
+
+    // req.host is an empty string
+    expect(testReq.host).toBe('')
+
+    // req.fresh is a boolean
+    expect(testReq.fresh).toBe(false)
+
+    // req.stale is a boolean
+    expect(testReq.stale).toBe(false)
+
+    // req.xhr is a boolean
+    expect(testReq.xhr).toBe(false)
+
+    // req.route is an empty object
+    expect(testReq.route).toBeTruthy()
+    expect(testReq.route).toBeInstanceOf(Object)
+    expect(Object.keys(testReq.route).length).toBe(0)
+
+    // req.signedCookies is an empty object
+    expect(testReq.signedCookies).toBeTruthy()
+    expect(testReq.signedCookies).toBeInstanceOf(Object)
+    expect(Object.keys(testReq.signedCookies).length).toBe(0)
+
+    // req.originalUrl is an empty string
+    expect(testReq.originalUrl).toBe('')
+
+    // req.url is an empty string
+    expect(testReq.url).toBe('')
+
+    // req.baseUrl is an empty string
+    expect(testReq.baseUrl).toBe('')
+
+    // req.accepted is an empty array
+    expect(Array.isArray(testReq.accepted)).toBe(true)
+    expect(testReq.accepted.length).toBe(0)
+
+    // req.get is a mocked function
+    expect((testReq.get as jest.Mock).getMockName()).toBe('get mock default')
+
+    // req.header is a mocked function
+    expect((testReq.header as jest.Mock).getMockName()).toBe('header mock default')
+
+    // req.accepts is a mocked function
+    expect((testReq.accepts as jest.Mock).getMockName()).toBe('accepts mock default')
+
+    // req.acceptsCharsets is a mocked function
+    expect((testReq.acceptsCharsets as jest.Mock).getMockName()).toBe('acceptsCharsets mock default')
+
+    // req.acceptsEncodings is a mocked function
+    expect((testReq.acceptsEncodings as jest.Mock).getMockName()).toBe('acceptsEncodings mock default')
+
+    // req.acceptsLanguages is a mocked function
+    expect((testReq.acceptsLanguages as jest.Mock).getMockName()).toBe('acceptsLanguages mock default')
+
+    // req.range is a mocked function
+    expect((testReq.range as jest.Mock).getMockName()).toBe('range mock default')
+
+    // req.param is a mocked function
+    expect((testReq.param as jest.Mock).getMockName()).toBe('param mock default')
+
+    // req.is is a mocked function
+    expect((testReq.is as jest.Mock).getMockName()).toBe('is mock default')
+
+    // req.app is an empty object
+    expect(testReq.app).toBeTruthy()
+    expect(testReq.app).toBeInstanceOf(Object)
+    expect(Object.keys(testReq.app).length).toBe(0)
+
+    // req.res is a mocked function
+    expect(((testReq.res as unknown) as jest.Mock).getMockName()).toBe('res mock default')
+
+    // req.next is a mocked function
+    expect((testReq.next as jest.Mock).getMockName()).toBe('next mock default')
   })
 
   test('returns provided params', () => {
     const testReq = getMockReq({ params: { one: 1, two: 2 } })
-
-    // the request contains values for params, query, and body
-    expect(testReq).toBeTruthy()
-    expect(testReq).toBeInstanceOf(Object)
-    expect(Object.keys(testReq).length).toBe(3)
 
     // req.params has the provided arguments
     expect(testReq.params).toBeTruthy()
@@ -40,30 +139,10 @@ describe('getMockReq', () => {
     expect(Object.keys(testReq.params).length).toBe(2)
     expect(testReq.params['one']).toBe(1)
     expect(testReq.params['two']).toBe(2)
-
-    // req.query is an empty object
-    expect(testReq.query).toBeTruthy()
-    expect(testReq.query).toBeInstanceOf(Object)
-    expect(Object.keys(testReq.query).length).toBe(0)
-
-    // req.body is an empty object
-    expect(testReq.body).toBeTruthy()
-    expect(testReq.body).toBeInstanceOf(Object)
-    expect(Object.keys(testReq.body).length).toBe(0)
   })
 
   test('returns provided query', () => {
     const testReq = getMockReq({ query: { three: 'three', four: 'four' } })
-
-    // the request contains values for params, query, and body
-    expect(testReq).toBeTruthy()
-    expect(testReq).toBeInstanceOf(Object)
-    expect(Object.keys(testReq).length).toBe(3)
-
-    // req.params is an empty object
-    expect(testReq.params).toBeTruthy()
-    expect(testReq.params).toBeInstanceOf(Object)
-    expect(Object.keys(testReq.params).length).toBe(0)
 
     // req.query has the provided arguments
     expect(testReq.query).toBeTruthy()
@@ -71,30 +150,10 @@ describe('getMockReq', () => {
     expect(Object.keys(testReq.query).length).toBe(2)
     expect(testReq.query['three']).toBe('three')
     expect(testReq.query['four']).toBe('four')
-
-    // req.body is an empty object
-    expect(testReq.body).toBeTruthy()
-    expect(testReq.body).toBeInstanceOf(Object)
-    expect(Object.keys(testReq.body).length).toBe(0)
   })
 
   test('returns provided body', () => {
     const testReq = getMockReq({ body: { five: { six: 6 }, seven: [8, 9] } })
-
-    // the request contains values for params, query, and body
-    expect(testReq).toBeTruthy()
-    expect(testReq).toBeInstanceOf(Object)
-    expect(Object.keys(testReq).length).toBe(3)
-
-    // req.params is an empty object
-    expect(testReq.params).toBeTruthy()
-    expect(testReq.params).toBeInstanceOf(Object)
-    expect(Object.keys(testReq.params).length).toBe(0)
-
-    // req.query is an empty object
-    expect(testReq.query).toBeTruthy()
-    expect(testReq.query).toBeInstanceOf(Object)
-    expect(Object.keys(testReq.query).length).toBe(0)
 
     // req.body has the provided arguments
     expect(testReq.body).toBeTruthy()
--- src/request.ts
+++ src/request.ts
@@ -1,11 +1,78 @@
 // Libraries
 import { Request } from 'express'
+import { MediaType } from 'express-serve-static-core'
 
-export const getMockReq = <T extends Request>(values?: { params?: any; query?: any; body?: any }): T =>
-  (({
+export const getMockReq = <T extends Request>(values?: {
+  params?: any
+  query?: any
+  body?: any
+  cookies?: any
+  method?: string
+  protocol?: string
+  secure?: boolean
+  ip?: string
+  ips?: string[]
+  subdomains?: string[]
+  path?: string
+  hostname?: string
+  host?: string
+  fresh?: boolean
+  stale?: boolean
+  xhr?: boolean
+  route?: any
+  signedCookies?: any
+  originalUrl?: string
+  url?: string
+  baseUrl?: string
+  accepted?: MediaType[]
+  get?: any
+  header?: any
+  accepts?: any
+  acceptsCharsets?: any
+  acceptsEncodings?: any
+  acceptsLanguages?: any
+  range?: any
+  param?: any
+  is?: any
+  app?: any
+  res?: any
+  next?: any
+}): T =>
+  ({
     params: (values && values.params) || {},
     query: (values && values.query) || {},
-    body: (values && values.body) || {}
-  } as unknown) as T)
+    body: (values && values.body) || {},
+    cookies: (values && values.cookies) || {},
+    method: (values && values.method) || '',
+    protocol: (values && values.protocol) || '',
+    secure: (values && values.secure) || false,
+    ip: (values && values.ip) || '',
+    ips: (values && values.ips) || [],
+    subdomains: (values && values.subdomains) || [],
+    path: (values && values.path) || '',
+    hostname: (values && values.hostname) || '',
+    host: (values && values.host) || '',
+    fresh: (values && values.fresh) || false,
+    stale: (values && values.stale) || false,
+    xhr: (values && values.xhr) || false,
+    route: (values && values.route) || {},
+    signedCookies: (values && values.signedCookies) || {},
+    originalUrl: (values && values.originalUrl) || '',
+    url: (values && values.url) || '',
+    baseUrl: (values && values.baseUrl) || '',
+    accepted: (values && values.accepted) || [],
+    get: (values && values.get) || jest.fn().mockName('get mock default'),
+    header: (values && values.header) || jest.fn().mockName('header mock default'),
+    accepts: (values && values.accepts) || jest.fn().mockName('accepts mock default'),
+    acceptsCharsets: (values && values.acceptsCharsets) || jest.fn().mockName('acceptsCharsets mock default'),
+    acceptsEncodings: (values && values.acceptsEncodings) || jest.fn().mockName('acceptsEncodings mock default'),
+    acceptsLanguages: (values && values.acceptsLanguages) || jest.fn().mockName('acceptsLanguages mock default'),
+    range: (values && values.range) || jest.fn().mockName('range mock default'),
+    param: (values && values.param) || jest.fn().mockName('param mock default'),
+    is: (values && values.is) || jest.fn().mockName('is mock default'),
+    app: (values && values.app) || {},
+    res: (values && values.res) || jest.fn().mockName('res mock default'),
+    next: (values && values.next) || jest.fn().mockName('next mock default')
+  } as T)
 
 export default getMockReq
