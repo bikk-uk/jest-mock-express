82999696315b2a7288de0897f33f157df72da7a8
aboyce <adam@aboyce.uk>
2020-06-03 20:55:53 +0100

    v1.1.0

--- dist/src/response/response.js
+++ dist/src/response/response.js
@@ -8,6 +8,7 @@ exports.getMockRes = void 0;
 exports.getMockRes = () => {
     const next = jest.fn();
     const response = {
+        // express - Response
         status: jest.fn(),
         sendStatus: jest.fn(),
         links: jest.fn(),
@@ -32,10 +33,38 @@ exports.getMockRes = () => {
         vary: jest.fn(),
         append: jest.fn(),
         headersSent: false,
+        locals: {},
+        charset: '',
         app: {},
         req: {},
+        // http - ServerResponse
+        assignSocket: jest.fn(),
+        detachSocket: jest.fn(),
+        writeContinue: jest.fn(),
+        writeHead: jest.fn(),
+        writeProcessing: jest.fn(),
+        statusCode: 0,
+        statusMessage: '',
+        // http - OutgoingMessage
+        setTimeout: jest.fn(),
+        setHeader: jest.fn(),
+        getHeader: jest.fn(),
+        getHeaders: jest.fn(),
+        getHeaderNames: jest.fn(),
+        hasHeader: jest.fn(),
+        removeHeader: jest.fn(),
+        addTrailers: jest.fn(),
+        flushHeaders: jest.fn(),
+        upgrading: false,
+        chunkedEncoding: false,
+        shouldKeepAlive: false,
+        useChunkedEncodingByDefault: false,
+        sendDate: false,
+        finished: false,
+        connection: {},
+        socket: {},
     };
-    // for the function that are chainable, return the response
+    // express - Response - chainable functions
     response.status.mockReturnValue(response);
     response.sendStatus.mockReturnValue(response);
     response.links.mockReturnValue(response);
@@ -53,17 +82,13 @@ exports.getMockRes = () => {
     response.location.mockReturnValue(response);
     response.vary.mockReturnValue(response);
     response.append.mockReturnValue(response);
-    /**
-     * these are not required to be chainable
-     * - get
-     * - render
-     * - sendFile
-     * - sendfile
-     * - download
-     * - redirect
-     */
+    // http - ServerResponse - chainable functions
+    response.writeHead.mockReturnValue(response);
+    // http - OutgoingMessage - chainable functions
+    response.setTimeout.mockReturnValue(response);
     const clearAllMocks = () => {
         next.mockClear();
+        // express - Response
         response.status.mockClear();
         response.sendStatus.mockClear();
         response.links.mockClear();
@@ -87,6 +112,22 @@ exports.getMockRes = () => {
         response.render.mockClear();
         response.vary.mockClear();
         response.append.mockClear();
+        // http - ServerResponse
+        response.assignSocket.mockClear();
+        response.detachSocket.mockClear();
+        response.writeContinue.mockClear();
+        response.writeHead.mockClear();
+        response.writeProcessing.mockClear();
+        // http - OutgoingMessage
+        response.setTimeout.mockClear();
+        response.setHeader.mockClear();
+        response.getHeader.mockClear();
+        response.getHeaders.mockClear();
+        response.getHeaderNames.mockClear();
+        response.hasHeader.mockClear();
+        response.removeHeader.mockClear();
+        response.addTrailers.mockClear();
+        response.flushHeaders.mockClear();
     };
     return {
         res: response,
