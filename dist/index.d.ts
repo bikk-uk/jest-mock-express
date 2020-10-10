9a9ff988eeb875806894cce862eed7b8efcae3d1
aboyce <adam@aboyce.uk>
2020-10-10 16:37:47 +0100

    Removed dist folder from repository.

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import getMockReq from './src/request/request';
-import getMockRes from './src/response/response';
-export { getMockReq, getMockRes };
--- dist/src/request/request.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request } from 'express';
-import { MockRequest } from './index';
-export declare const getMockReq: <T extends Request<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>>(values?: MockRequest) => T;
-export default getMockReq;
